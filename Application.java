474765b5c57cf317bb18666b13ef1d302b266377
bmroberts01 <bmroberts01@bvsd.org>
2026-09-12 01:25:15 +0000

    It Times

--- Application.java
+++ Application.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+/**
+    Blake Roberts
+    08/27/26 
+**/
 public class Application
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter amount of seconds:   ");
+        int seconds = keyboard.nextInt();
+
+        int hours = seconds / 3600;
+        seconds = seconds - (hours * 3600);
+
+        int minutes = seconds / 60;
+        seconds = seconds - (minutes * 60);
+        
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+
+
+        System.out.println("Enter a value in cents to simplify");
+        int cents = keyboard.nextInt();
+
+        int quarters = cents / 25;
+        cents = cents - (quarters * 25);
+
+        int dimes = cents / 10;
+        cents = cents - (dimes * 10);
+
+        int nickels = cents / 5;
+        cents = cents - (cents * 5);
+
+        System.out.println( quarters + " Quarters, " + dimes + " Dimes," + nickels + " Nickels, " + "and" + cents + " Cents");
+
+
+         keyboard.close();
+
     }
 }
